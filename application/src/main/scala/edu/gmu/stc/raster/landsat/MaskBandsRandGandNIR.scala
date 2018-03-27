7b46cd0f6c8c24c3cefc14003be2198805468810
Fei Hu <hufei68@gmail.com>
2018-03-27 12:01:51 -0400

    Combine more bands into tiff

--- application/src/main/scala/edu/gmu/stc/raster/landsat/MaskBandsRandGandNIR.scala
+++ application/src/main/scala/edu/gmu/stc/raster/landsat/MaskBandsRandGandNIR.scala
@@ -4,15 +4,17 @@ import geotrellis.raster._
 import geotrellis.raster.io.geotiff._
 
 object MaskBandsRandGandNIR {
-  val maskedPath = "data/r-g-nir.tif"
+  val maskedPath = "data/r-g-nir-tirs1-swir1.tif"
   //constants to differentiate which bands to use
   val R_BAND = 0
   val G_BAND = 1
   val NIR_BAND = 2
-  val TIRS_BAND = 3
+  val TIRS1_BAND = 3
+  val SWIR1_BAND = 4
+
   // Path to our landsat band geotiffs.
   //def bandPath(b: String) = s"data/landsat/LC81070352015218LGN00_${b}.TIF"
-  def bandPath(b: String) = s"data/landsat/LC08_L1TP_015033_20170822_20170822_01_RT_${b}.TIF"
+  def bandPath(b: String) = s"data/landsat8_dc/LC08_L1TP_015033_20170822_20170822_01_RT_${b}.TIF"
 
   def main(args: Array[String]): Unit = {
     // Read in the red band
@@ -23,20 +25,25 @@ object MaskBandsRandGandNIR {
     println("Reading in green band...")
     val gGeoTiff = SinglebandGeoTiff(bandPath("B3"))
 
-    // Read in the near infrared band
+    // Read in the near infrared band (NIR)
     println("Reading in the NIR band...")
     val nirGeoTiff = SinglebandGeoTiff(bandPath("B5"))
+
+    // Read in the TIRS1 (band10) band
+    println("Reading in the TIRS band/band10")
+    val tirs1GeoTiff = SinglebandGeoTiff(bandPath("B10"))
+
+    // Read in the TIRS1 (band10) band
+    println("Reading in the SWIR1")
+    val swir1GeoTiff = SinglebandGeoTiff(bandPath("B6"))
 
     // Read in the QA band
     println("Reading in the QA band...")
     val qaGeoTiff = SinglebandGeoTiff(bandPath("BQA"))
 
-    // Read in the TIRS (band10) band
-    println("Reading in the TIRS band/band10")
-    val tirsGeoTiff = SinglebandGeoTiff(bandPath("B10"))
-
     // GeoTiffs have more information we need; just grab the Tile out of them.
-    val (rTile, gTile, nirTile, qaTile, tirsTile) = (rGeoTiff.tile, gGeoTiff.tile, nirGeoTiff.tile, qaGeoTiff.tile, tirsGeoTiff.tile)
+    val (rTile, gTile, nirTile, qaTile, tirs1Tile, swir1Tile) = (rGeoTiff.tile, gGeoTiff.tile,
+      nirGeoTiff.tile, qaGeoTiff.tile, tirs1GeoTiff.tile, swir1GeoTiff.tile)
 
     // This function will set anything that is potentially a cloud to NODATA
     def maskClouds(tile: Tile): Tile =
@@ -55,13 +62,15 @@ object MaskBandsRandGandNIR {
     println("Masking clouds in the NIR band...")
     val nirMasked = maskClouds(nirTile)
     println("Masking clouds in the TIRS band...")
-    val tirsMasked = maskClouds(tirsTile)
+    val tirs1Masked = maskClouds(tirs1Tile)
+    println("Masking clouds in the SWIR1 band...")
+    val swir1Masked = maskClouds(swir1Tile)
 
     // Create a multiband tile with our two masked red and infrared bands.
-    val mb = ArrayMultibandTile(rMasked, gMasked, nirMasked, tirsMasked).convert(IntConstantNoDataCellType)
+    val mb = ArrayMultibandTile(rMasked, gMasked, nirMasked, tirs1Masked, swir1Masked).convert(IntConstantNoDataCellType)
 
     // Create a multiband geotiff from our tile, using the same extent and CRS as the original geotiffs.
-    println("Writing out the multiband R + G + NIR + TIRS tile...")
+    println("Writing out the multiband R + G + NIR + TIRS1 + SWIR1 tile...")
     MultibandGeoTiff(mb, rGeoTiff.extent, rGeoTiff.crs).write(maskedPath)
   }
 }
